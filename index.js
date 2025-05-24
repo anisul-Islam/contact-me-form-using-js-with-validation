c6994eff2e50b937acb843e06723ede0928b612d
anisul-islam <anisul2010s@yahoo.co.uk>
2025-05-24 15:34:41 +0300

    finished the contact me form with live validation

--- index.js
+++ index.js
@@ -1,51 +1,85 @@
 //@ts-nocheck
 
 const contactForm = document.getElementById('contactForm');
-const nameElement = contactForm?.querySelector('#name');
-const email = contactForm?.querySelector('#email');
-const message = contactForm?.querySelector('#message');
-const feedback = document.querySelector('#feedback');
 
-// Simple email pattern
-const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+if (contactForm) {
+  const fields = {
+    name: contactForm?.querySelector('#name'),
+    email: contactForm?.querySelector('#email'),
+    message: contactForm?.querySelector('#message'),
+  };
+  const errors = {
+    name: contactForm?.querySelector('#nameError'),
+    email: contactForm?.querySelector('#emailError'),
+    message: contactForm?.querySelector('#messageError'),
+  };
 
-contactForm?.addEventListener('submit', handleSubmit);
+  const feedbackElement = document.querySelector('#feedback');
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-function handleSubmit(event) {
-  event.preventDefault();
+  // Attach event listener
+  contactForm?.addEventListener('submit', handleSubmit);
+  fields.name.addEventListener('input', validateName);
+  fields.email.addEventListener('input', validateEmail);
+  fields.message.addEventListener('input', validateMessage);
 
-  const nameValue = nameElement.value.trim();
-  const emailValue = email.value.trim();
-  const messageValue = message.value.trim();
+  function validateName() {
+    const value = fields.name.value.trim();
+    if (value === '') {
+      errors.name.textContent = 'Name is required';
+      return false;
+    } else {
+      errors.name.textContent = '';
+      return true;
+    }
+  }
+  function validateEmail() {
+    const value = fields.email.value.trim();
+    if (!emailPattern.test(value)) {
+      errors.email.textContent = 'Please enter a valid email.';
+      return false;
+    } else {
+      errors.email.textContent = '';
+      return true;
+    }
+  }
+  function validateMessage() {
+    const value = fields.message.value.trim();
 
-  // const userInfo = {
-  //   name: nameValue,
-  //   email: emailValue,
-  //   message: messageValue,
-  // };
-
-  // validation
-  if (nameValue === '' || emailValue === '' || messageValue === '') {
-    feedback.textContent = 'Please fill in all fields.';
-    feedback.style.color = '#d32f2f';
-    return;
+    if (value.length < 10) {
+      errors.message.textContent = 'Message should be atleast 10 characters.';
+      return false;
+    } else {
+      errors.message.textContent = '';
+      return true;
+    }
   }
 
-  if (!emailPattern.test(emailValue)) {
-    feedback.textContent = 'Please enter a valid email.';
-    feedback.style.color = '#d32f2f';
-    return;
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    let isNameValid = validateName();
+    let isEmailValid = validateEmail();
+    let isMessageValid = validateMessage();
+
+    const isValid = isNameValid && isEmailValid && isMessageValid;
+
+    // validation
+    if (isValid) {
+      const userInfo = {
+        name: fields.name.value,
+        email: fields.email.value,
+        message: fields.message.value,
+      };
+      console.log(userInfo);
+
+      // if all the validation pass
+      feedbackElement.textContent = `Thank you, ${userInfo.name}! Your message has been sent.`;
+      feedbackElement.style.color = '#388e3c';
+      contactForm?.reset();
+    } else {
+      feedbackElement.style.color = '#d32f2f';
+      feedbackElement.textContent = 'Please fix the errors above';
+    }
   }
-
-  if (messageValue.length < 10) {
-    feedback.textContent = 'Message should be atleast 10 characters.';
-    feedback.style.color = '#d32f2f';
-    return;
-  }
-
-  // if all the validation pass
-  feedback.textContent = `Thank you, ${nameValue}! Your message has been sent.`;
-  feedback.style.color = '#388e3c';
-
-  contactForm?.reset();
 }
